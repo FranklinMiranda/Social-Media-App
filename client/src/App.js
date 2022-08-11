6e6ad6eb4dc732f14394854c8ca260ad99836a61
FranklinMiranda <fdm61344@gmail.com>
2022-08-11 02:29:38 -0400

    CommitNewUpdates

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import React, {useEffect} from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+
+import {getPosts} from './actions/posts';
+import Posts from './components/Posts/posts';
+import Form from './components/Form/form';
+import ollie from './images/ollie.jpeg';
+import useStyles from './styles';
 
 const App = () => {
-    return (
-       <Container maxwidth='lg'>
-        <AppBar position="static" color="inherit">
-            <Typography variant="h2" align="center">Social Media App</Typography>
-            <img src={memories} alt="memories" height="60"></img>
-        </AppBar>
-       </Container>
-    );
-    }
+  const classes = useStyles();
+    const dispatch = useDispatch();
 
-    export default App;
-    
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
+
+  return (
+    <Container maxwidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Social Media App
+        </Typography>
+        <img className={classes.image} src={ollie} alt="Ollie" height="150"></img>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+
+            <Grid item xs={12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
+};
+
+export default App;
