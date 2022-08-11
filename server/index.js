454376f98bc9f527a4da130ff8176be1cd7ddd3b
FranklinMiranda <fdm61344@gmail.com>
2022-08-10 23:24:23 -0400

    update

--- server/index.js
+++ server/index.js
@@ -3,5 +3,17 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
-const express = require('express');
+const app = express();
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECT_URL = 'mongodb+srv://user:user123@cluster0.uti46og.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECT_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen([PORT, () => console.log(`Server running on port: ${PORT}`)]))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
