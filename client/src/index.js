43f220e51738b732dae43af38e4a7c47ca538f97
FranklinMiranda <fdm61344@gmail.com>
2022-08-12 18:28:37 -0400

    refreshCommit

--- client/src/index.js
+++ client/src/index.js
@@ -9,12 +9,6 @@ import reducers from './reducers';
 import App from './app';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-const store = createStore(reducers, compose(applyMiddleware(thunk)));
-
-
 
 
 ReactDOM.render(
